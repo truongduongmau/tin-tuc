54d7f0507ebcd766bd02eb25495fe81a3f2ba871
truongduongmau <truongduongmau@gmail.com>
2023-10-12 23:32:25 +0700

    update

--- src/env/env.prod.ts
+++ src/env/env.prod.ts
@@ -1,4 +1,4 @@
 export const env = {
     production: true,
-    apiURL: 'https://thingproxy.freeboard.io/fetch/https://cafef.vn'
+    apiURL: 'https://api.allorigins.win/raw?url=https://cafef.vn'
 };
