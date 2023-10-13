0ae93325420941770734e709ffd68f5bf86d7e4e
truongduongmau <truongduongmau@gmail.com>
2023-10-13 22:01:00 +0700

    update

--- src/app/homes/home-main/home-main.component.ts
+++ src/app/homes/home-main/home-main.component.ts
@@ -1,44 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { HttpService } from '../services/http.service';
+import { exhaustMap } from 'rxjs/internal/operators/exhaustMap';
+import { Subject } from 'rxjs/internal/Subject';
 
 @Component({
     selector: 'app-home-main',
     templateUrl: './home-main.component.html',
     styleUrls: ['./home-main.component.css']
 })
-export class HomeMainComponent implements OnInit {
-    fastnews_main: string = "";
-    page: number = 0;
+export class HomeMainComponent implements OnInit, AfterViewInit {
+    page: number = 1;
     isLoading: boolean = false;
+    fastnews_main: any;
+    callApi$: Subject<boolean> = new Subject();
 
     constructor(private httpService: HttpService) { }
 
     ngOnInit(): void {
+        this.callApi$.pipe(exhaustMap(() => this.httpService.getContents(this.page)))
+            .subscribe(
+                (data: any) => {
+                    this.page += 1;
+                    this.isLoading = false;
+                    let dom_document = new DOMParser().parseFromString(data, "text/html");
+                    const news = dom_document.querySelectorAll("div.list-fast-news > .item");
+
+                    news.forEach((item) => {
+                        let timeTitle = item.querySelector("div.timeTitle")
+                        const time = item.querySelector("span.time");
+                        const timeText = (time?.innerHTML || "").replace("(", "").replace(")", "");
+                        timeTitle?.replaceChildren(timeText);
+
+                        let title = item.querySelector(".title-wrap > a")
+                        const href = title?.getAttribute("href")
+                        title?.setAttribute("href", "https://cafef.vn" + href)
+                        this.fastnews_main?.append(item)
+                    })
+                },
+                () => {
+                    this.isLoading = false;
+                });
+    }
+
+    ngAfterViewInit() {
+        this.fastnews_main = document.getElementById("fastnews-main-contents") as Element;
         this.getData()
     }
 
     getData() {
-        this.page += 1;
         this.isLoading = true;
-        this.httpService.getContents(this.page)
-            .subscribe((data: any) => {
-                this.isLoading = false;
-                let fastnews_main = document.getElementById("fastnews-main-contents")
-                let dom_document = new DOMParser().parseFromString(data, "text/html");
-                let news = dom_document.querySelectorAll("div.list-fast-news > .item");
-                news.forEach((item) => {
-                    let timeTitle = item.querySelector("div.timeTitle")
-                    let title = item.querySelector(".title-wrap > a")
-                    const time = item.querySelector("span.time");
-                    const timeText = (time?.innerHTML || "").replace("(", "").replace(")", "");
-                    timeTitle?.replaceChildren(timeText);
-                    const href = title?.getAttribute("href")
-                    title?.setAttribute("href", "https://cafef.vn" + href)
-                    fastnews_main?.append(item)
-                })
-            },
-            () => {
-                this.isLoading = false;
-            });
+        this.callApi$.next(true);
+    }
+
+    ngOnDestroy(): void {
+        this.callApi$.unsubscribe()
     }
 }
