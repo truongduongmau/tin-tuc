5bff7515e7c8e1d686735ffaf4966b0d355de184
truongduongmau <truongduongmau@gmail.com>
2023-12-20 19:01:18 +0700

    update

--- src/env/env.prod.ts
+++ src/env/env.prod.ts
@@ -1,5 +1,6 @@
 export const env = {
     production: true,
     //apiURL: 'https://api.allorigins.win/raw?url=https://cafef.vn'
-    apiURL: 'http://103.179.190.146:8080/https://cafef.vn'
+    //apiURL: 'http://103.179.190.146:8080/https://cafef.vn'
+    apiURL: 'https://cors-anywhere.herokuapp.com/http://103.179.190.146:8080/https://cafef.vn'
 };
