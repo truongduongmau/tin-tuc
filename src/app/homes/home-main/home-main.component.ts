c5c36951b479713f286d1528b10587f57a8f8164
truongduongmau <truongduongmau@gmail.com>
2023-12-21 21:35:07 +0700

    update

--- src/app/homes/home-main/home-main.component.ts
+++ src/app/homes/home-main/home-main.component.ts
@@ -2,6 +2,8 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { HttpService } from '../services/http.service';
 import { exhaustMap } from 'rxjs/internal/operators/exhaustMap';
 import { Subject } from 'rxjs/internal/Subject';
+import { catchError, finalize, mergeMap, retryWhen, switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
     selector: 'app-home-main',
@@ -12,34 +14,33 @@ export class HomeMainComponent implements OnInit, AfterViewInit {
     page: number = 1;
     isLoading: boolean = false;
     fastnews_main: any;
-    callApi$: Subject<boolean> = new Subject();
+    callApi$: Subject<string> = new Subject();
+    hostIndex: number = 0;
+    hosts = [
+        'https://api.allorigins.win/raw?url=https://cafef.vn',
+        'https://thingproxy.freeboard.io/fetch/https://cafef.vn',
+        'https://cors-anywhere.herokuapp.com/http://103.179.190.146:8080/https://cafef.vn',
+    ]
 
     constructor(private httpService: HttpService) { }
 
     ngOnInit(): void {
-        this.callApi$.pipe(exhaustMap(() => this.httpService.getContents(this.page)))
+        this.callApi$
+            .pipe(
+                exhaustMap((apiUrl) => this.httpService.getContents(apiUrl, this.page)),
+                catchError(() => {
+                    this.hostIndex++;
+                    return this.httpService.getContents(this.hosts[this.hostIndex], this.page);
+                }),
+            )
             .subscribe(
                 (data: any) => {
-                    this.page += 1;
-                    this.isLoading = false;
-                    let dom_document = new DOMParser().parseFromString(data, "text/html");
-                    const news = dom_document.querySelectorAll("div.list-fast-news > .item");
-
-                    news.forEach((item) => {
-                        let timeTitle = item.querySelector("div.timeTitle")
-                        const time = item.querySelector("span.time");
-                        const timeText = (time?.innerHTML || "").replace("(", "").replace(")", "");
-                        timeTitle?.replaceChildren(timeText);
-
-                        let title = item.querySelector(".title-wrap > a")
-                        const href = title?.getAttribute("href")
-                        title?.setAttribute("href", "https://cafef.vn" + href)
-                        this.fastnews_main?.append(item)
-                    })
+                    this.getDataSuccess(data);
                 },
                 () => {
                     this.isLoading = false;
-                });
+                }
+            );
     }
 
     ngAfterViewInit() {
@@ -49,10 +50,45 @@ export class HomeMainComponent implements OnInit, AfterViewInit {
 
     getData() {
         this.isLoading = true;
-        this.callApi$.next(true);
+        this.hostIndex = 0;
+        //this.callApi$.next(this.hosts[this.hostIndex]);
+        this.getContents(this.hosts[this.hostIndex])
+    }
+
+    getDataSuccess(data: string) {
+        this.page += 1;
+        this.isLoading = false;
+        let dom_document = new DOMParser().parseFromString(data, "text/html");
+        const news = dom_document.querySelectorAll("div.list-fast-news > .item");
+
+        news.forEach((item) => {
+            let timeTitle = item.querySelector("div.timeTitle")
+            const time = item.querySelector("span.time");
+            const timeText = (time?.innerHTML || "").replace("(", "").replace(")", "");
+            timeTitle?.replaceChildren(timeText);
+
+            let title = item.querySelector(".title-wrap > a")
+            const href = title?.getAttribute("href")
+            title?.setAttribute("href", "https://cafef.vn" + href)
+            this.fastnews_main?.append(item)
+        })
     }
 
     ngOnDestroy(): void {
         this.callApi$.unsubscribe()
+    }
+
+    getContents(apiUrl: string) {
+        this.httpService.getContents(apiUrl, this.page).subscribe(
+            (data: any) => {
+                this.getDataSuccess(data);
+            },
+            () => {
+                this.hostIndex++;
+                if (this.hosts[this.hostIndex]) {
+                    this.getContents(this.hosts[this.hostIndex])
+                }
+            }
+        )
     }
 }
--- src/app/homes/services/http.service.ts
+++ src/app/homes/services/http.service.ts
@@ -9,13 +9,12 @@ export class HttpService {
 
     constructor(private http: HttpClient) { }
 
-    getContents(page: number) {
+    getContents(apiUrl: string, page: number) {
         const salt = (new Date()).getTime();
-        return this.http.get(`${env.apiURL}/doc-nhanh/trang-${page}.chn?${salt}`, {
+        return this.http.get(`${apiUrl}/doc-nhanh/trang-${page}.chn?${salt}`, {
             responseType: "text", 
             headers: {
                 "X-Requested-With": "XMLHttpRequest",
-                //'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
             }
         });
     }
